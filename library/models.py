1abbe51ab8c4890efef7657752cb465be0dd87d3
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-19 12:03:12 -0500

    fix: add inheritance between model and partner

--- library/models.py
+++ library/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Partner():
+class Partner(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     date_birth = models.DateField()
@@ -10,4 +10,5 @@ class Partner():
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
-    
+
+
