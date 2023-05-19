da8225a59967ba8ea4e1d9eaf370e792a05038e3
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-19 12:03:29 -0500

    feat: book model added

--- library/models.py
+++ library/models.py
@@ -11,4 +11,8 @@ class Partner(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
-
+class Book(models.Model):
+    title = models.TextField(max_length=255)
+    description = models.TextField(max_length=255)
+    ISBN = models.IntegerField(max_length=13)
+    active = models.BooleanField(default=True)
