c944e22682a5452f4f0f6ffda4bd5718fdc8a3de
deheredia <118789041+deheredia@users.noreply.github.com>
2023-05-21 20:08:34 -0300

    models Employee (Clase Empleado)

--- library/models.py
+++ library/models.py
@@ -21,4 +21,16 @@ class Author(models.Model):
     def __str__(self):
         return self.name
     
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=50)
+    numero_legajo = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.surname
+    
+
+    
     
