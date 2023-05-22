43abacd5ca8e9d12c7db77eddce1480b1cbc4795
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-22 16:07:08 -0500

    feat: Book loan admin added

--- library/admin.py
+++ library/admin.py
@@ -1,18 +1,39 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book,BookLoan
 
 # Register your models here.
 
+
 class BookAdmin(admin.ModelAdmin):
-    
     # Lista display, muestra los atributos
-    list_display = ('title', 'description', 'ISBN', 'author', 'active',)
-    
+    list_display = (
+        "title",
+        "description",
+        "ISBN",
+        "author",
+        "active",
+    )
+
     # Filtrar los objetos
-    list_filter = ('active',)
+    list_filter = ("active",)
 
     # Buscar por campos especificados
-    search_fields = ('title',)
+    search_fields = ("title",)
+
+
+class BookLoanAdmin(admin.ModelAdmin):
+    # Lista display, muestra los atributos
+    list_display = (
+        "partner",
+        "employee",
+        "book",
+        "loan_date",
+        "return_date",
+    )
+
+    # Buscar por campos especificados
+    search_fields = ("partner","book","employee")
+
 
 admin.site.register(Book, BookAdmin)
-
+admin.site.register(BookLoan, BookLoanAdmin)
