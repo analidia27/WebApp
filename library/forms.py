819aa84e3be76aff7e7cd31852c1ca5985aef480
analidia27 <anamechaca27@gmail.com>
2023-05-23 21:02:48 -0300

    template listado empleado
    Se agrega template de listado de empleados
    Se modfico clase Employee Admin para que los labels esten en español

--- library/forms.py
+++ library/forms.py
@@ -6,4 +6,10 @@ class EmployeeForm(forms.ModelForm):
     class Meta:
         model = Employee
         fields = '__all__'
+        labels = {
+            'name': 'Nombre',
+            'surname': 'Apellido',
+            'numero_legajo': 'Número de legajo',
+            'is_active': 'Activo'
+        }
 
