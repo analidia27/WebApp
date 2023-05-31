fd5f777050540ec1e5f30234d349ec3ce6320f3a
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-31 10:26:27 -0500

    fix: create without checkbox is_active and datefield

--- library/forms.py
+++ library/forms.py
@@ -5,56 +5,36 @@ from .models import Employee, Author,Partner
 class EmployeeForm(forms.ModelForm):
     class Meta:
         model = Employee
-        fields = '__all__'
+        fields = ['name','surname','numero_legajo']
         labels = {
             'name': 'Nombre',
             'surname': 'Apellido',
-            'numero_legajo': 'Número de legajo',
-            'is_active': 'Activo'
+            'numero_legajo': 'Número de legajo'
         }
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        instance = kwargs.get('instance')
-
-        if instance:
-            # Si existe una instancia, ocultar el campo is_active durante la actualización
-            self.fields['is_active'].widget = forms.HiddenInput()
 
 class AuthorForm(forms.ModelForm):
     class Meta:
         model = Author
-        fields = '__all__'
+        fields = ['name','surname','nationality']
         labels = {
                 'name': 'Nombre',
                 'surname': 'Apellido',
-                'nationality': 'Nacionalidad',
-                'is_active': 'Activo'
+                'nationality': 'Nacionalidad'
             }
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        instance = kwargs.get('instance')
 
-        if instance:
-            # Si existe una instancia, ocultar el campo is_active durante la actualización
-            self.fields['is_active'].widget = forms.HiddenInput()
 
 
 class PartnerForm(forms.ModelForm):
     class Meta:
         model = Partner
-        fields = '__all__'
+        fields = ['first_name','last_name','date_birth']
         labels = {
                 'first_name': 'Nombre',
                 'last_name': 'Apellido',
-                'date_birth': 'Fecha de Nacimiento',
-                'is_active': 'Activo'
+                'date_birth': 'Fecha de Nacimiento'
             }
+        widgets = {
+            'date_birth': forms.DateInput(attrs={'type': 'date'})
+        }
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        instance = kwargs.get('instance')
-
-        if instance:
-            # Si existe una instancia, ocultar el campo is_active durante la actualización
-            self.fields['is_active'].widget = forms.HiddenInput()
