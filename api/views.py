0d94a2204d4efec4b2028a007b8922db67947f83
Ana Mechaca <31392704+analidia27@users.noreply.github.com>
2023-06-02 18:08:17 -0300

    Revert "Sc5 s4 74"

--- api/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from library.models import Book
-
-def list_books(request):
-    books = Book.objects.all().values('id','title','author')
-    context = {
-        'books' : books
-    }
-    return render(request, 'list_book.html', context=context)
