55f18b38694dbc0322b8ff6f9144d286115897e0
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-06-07 15:28:45 -0500

    feat: view of employees

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('api/libros', views.list_books_json, name='api_libros'),
     path('api/libros/<int:id>', views.book_json, name='api_libro'),
     path('api/socios', views.list_partner_json, name='api_socios'),
+    path('api/empleados', views.list_employee_json, name='api_socios'),
 ]
