1ad975e37bce8d984951fe4cfe36041cab54e9c5
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-06-08 09:54:25 -0500

    fix: get emloyees in api

--- api/views.py
+++ api/views.py
@@ -4,105 +4,117 @@ from django.core.serializers import serialize
 from django.shortcuts import render
 from library.models import Book, Author, Partner, Employee
 
+
 def list_books_json(request):
-    books = Book.objects.all().values('id', 'title','author')
-    if(books):
+    books = Book.objects.all().values("id", "title", "author")
+    if books:
         list_books = list()
-        for book in books:  
+        for book in books:
             item = list(book.values())
             temp_book = {}
-            temp_book['id'] = item[0]
-            temp_book['titulo'] = item[1]
-            temp_book['autor'] = str(Author.objects.get(id=item[2]))
+            temp_book["id"] = item[0]
+            temp_book["titulo"] = item[1]
+            temp_book["autor"] = str(Author.objects.get(id=item[2]))
             list_books.append(temp_book)
 
     else:
         list_books = list()
-    data = {
-        'libros': list_books
-    }
-    return HttpResponse(json.dumps(data, ensure_ascii=False).encode('utf-8'), content_type="application/json") 
-
+    data = {"libros": list_books}
+    return HttpResponse(
+        json.dumps(data, ensure_ascii=False).encode("utf-8"),
+        content_type="application/json",
+    )
 
 
 def book_json(request, id):
-
-    if request.method == 'GET':
+    if request.method == "GET":
         try:
-
-            book = Book.objects.get(id = id)
+            book = Book.objects.get(id=id)
 
             data = {
-                'Libro': {
-                    'id' : id,
-                    'titulo' : book.title,
-                    'descripcion' : book.description,
-                    'autor' : str(book.author)
-                    }
+                "Libro": {
+                    "id": id,
+                    "titulo": book.title,
+                    "descripcion": book.description,
+                    "autor": str(book.author),
+                }
             }
 
             return JsonResponse(data)
-        
+
         except:
-            msj = {
-                'Libro' : []
-                }
+            msj = {"Libro": []}
 
             return JsonResponse(msj)
-        
+
+
 def list_partner_json(request):
-    partners = Partner.objects.all().values('id', 'first_name','last_name','date_birth','is_active')
-    if(partners):
+    partners = Partner.objects.all().values(
+        "id", "first_name", "last_name", "date_birth", "is_active"
+    )
+    if partners:
         list_partners = list()
-        for partner in partners:  
+        for partner in partners:
             item = list(partner.values())
             temp_partner = {}
-            temp_partner['id'] = item[0]
-            temp_partner['first_name'] = item[1]
-            temp_partner['last_name'] = item[2]
-            temp_partner['date_birth'] = str(item[3])
-            temp_partner['is_active'] = item[4]
+            temp_partner["id"] = item[0]
+            temp_partner["first_name"] = item[1]
+            temp_partner["last_name"] = item[2]
+            temp_partner["date_birth"] = str(item[3])
+            temp_partner["is_active"] = item[4]
             list_partners.append(temp_partner)
 
     else:
         list_partners = list()
-    data = {
-        'socios': list_partners
-    }
-    return HttpResponse(json.dumps(data, ensure_ascii=False).encode('utf-8'), content_type="application/json")
+    data = {"socios": list_partners}
+    return HttpResponse(
+        json.dumps(data, ensure_ascii=False).encode("utf-8"),
+        content_type="application/json",
+    )
+
 
 def list_employee_json(request):
     employees = Employee.objects.all()
     if employees:
         list_employees = list()
         for employee in employees:
-            list_employees.append(employee.__dict__())
+            aux = {
+                "name": employee.name,
+                "surname": employee.surname,
+                "numero_legajo": employee.numero_legajo,
+                "is_active": employee.is_active,
+            }
+            list_employees.append(aux)
 
     else:
         list_employees = list()
-    data = {
-        'empleados': list_employees
-    }
-    return HttpResponse(json.dumps(data, ensure_ascii=False).encode('utf-8'), content_type="application/json")
+    data = {"empleados": list_employees}
+    return HttpResponse(
+        json.dumps(data, ensure_ascii=False).encode("utf-8"),
+        content_type="application/json",
+    )
 
 
 def list_author_json(request):
-    authors = Author.objects.all().values('id','name', 'surname','nationality','is_active')
-    if(authors):
+    authors = Author.objects.all().values(
+        "id", "name", "surname", "nationality", "is_active"
+    )
+    if authors:
         list_authors = list()
-        for author in authors:  
+        for author in authors:
             item = list(author.values())
             temp_author = {}
-            temp_author['id'] = item[0]
-            temp_author['nombre'] = item[1]
-            temp_author['apellido'] = item[2]
-            temp_author['nacionalidad'] = item[3]
-            temp_author['activo'] = item[4]
+            temp_author["id"] = item[0]
+            temp_author["nombre"] = item[1]
+            temp_author["apellido"] = item[2]
+            temp_author["nacionalidad"] = item[3]
+            temp_author["activo"] = item[4]
             list_authors.append(temp_author)
 
     else:
         list_authors = list()
-    data = {
-        'autores': list_authors
-    }
-    return HttpResponse(json.dumps(data, ensure_ascii=False).encode('utf-8'), content_type="application/json") 
+    data = {"autores": list_authors}
+    return HttpResponse(
+        json.dumps(data, ensure_ascii=False).encode("utf-8"),
+        content_type="application/json",
+    )
